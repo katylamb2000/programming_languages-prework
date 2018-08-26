0fbae79419832597786572527a2d8ba0cb767d7d
katy@karstaway.com <katy@karstaway.com>
2018-08-26 14:58:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,12 @@
 def reformat_languages(languages)
   new_hash = {}
-  languages.collect do |type|
-    new_hash[:type] = type
+  
+  
+    languages.each_with_object({}) do |(k, v), inverted_languages|
+        if k.is_a? Hash
+            k = recursive_invert(k)
+        end
+        inverted_languages[v] = k 
+    end
 end
-end
+
